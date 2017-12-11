8691fcaefa1beb8b0ba426d819ffce7c28529245
Fabien Rdt <rodet.fabien@gmail.com>
2017-12-11 17:43:02 +0100

    V.11-Dec.-17

--- amap/src/index.js
+++ amap/src/index.js
@@ -1,1 +1,8 @@
 // c'est parti !
+import React from 'react';
+import { render } from 'react-dom';
+//import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css';
+
+render (<App />, document.querySelector('#main'));
